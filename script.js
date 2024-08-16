4502af45c7306d4dd04a9857d71c2871a504b330
Prakhar Doneria <gamesprotec@gmail.com>
2024-08-16 09:32:14 +0000

    Final

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ paypal.Buttons({
     },
     onApprove: function(data, actions) {
         return actions.order.capture().then(function(details) {
-            window.location.href = '/success'; // Redirect to success page
+            window.location.href = '/success';
         });
     },
     onError: function(err) {
-        window.location.href = '/error'; // Redirect to error page
+        window.location.href = '/error'; 
     }
 }).render('#paypal-button-container');
